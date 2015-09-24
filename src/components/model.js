53a93a6f1647cd4be36d3711a119f7d5c78fc8b2
Jura Studenkov <jurstv@gmail.com>
2015-09-25 02:50:52 +0300

    v1.2.4
    got rid of Map store in favor of Observable.prototype.scan

--- src/components/model.js
+++ src/components/model.js
@@ -1,14 +1,38 @@
 import last from "ramda/src/last";
+import append from "ramda/src/append";
+import {Rx} from "@cycle/core";
+
+function accumulator( accValue, current) {
+  return (current.actionType === "add") ?
+      {...accValue, list: append(current.item, accValue.list)} :
+       (current.actionType === "remove") ?
+      {...accValue, list: accValue.list.filter( x => x._id !== current.item._id)} :
+      accValue ;
+}
+
 export default function model(sourceStore$, delete$, submit$ ){
-  return sourceStore$.flatMap( ( storeImm ) => {
-    let mapMutable = storeImm.list.reduce( (acc, cur) => acc.set(cur._id, cur) ,new Map());
-		let deleteFromList$ = delete$.map( actAdr =>
-			   mapMutable.delete( actAdr._id ) ?
-          [...mapMutable].map( last ) : null
-		 ).filter( x => !!x );
-		let pushToList$ = submit$.map( actAdr =>
-			  [...mapMutable.set(actAdr._id, actAdr)].map( last )
-		 );
-		return  deleteFromList$.merge(pushToList$).map( x => ({list: x}) ).throttle(100);
-	} );
+  let removeFromList$ = delete$.map( item => ({item,actionType: "remove"}) );
+  let pushToList$ = submit$.map( item => ({item,actionType: "add"}) );
+  let modifyList$ = removeFromList$.merge(pushToList$);
+  return sourceStore$.flatMap( initialStore =>
+      modifyList$.scan(accumulator, initialStore).throttle(100)
+   );
+
+
+  // return sourceStore$.flatMap( ( storeImm ) => {
+  //
+  //   let mapMutable = storeImm.list.reduce( (acc, cur) => acc.set(cur._id, cur) ,new Map());
+  //
+	// 	let deleteFromList$ = delete$.map( actAdr =>
+	// 		   mapMutable.delete( actAdr._id ) ?
+  //         [...mapMutable].map( last ) : null
+	// 	 ).filter( x => !!x );
+  //
+	// 	let pushToList$ = submit$.map( actAdr =>
+	// 		  [...mapMutable.set(actAdr._id, actAdr)].map( last )
+	// 	 );
+  //
+	// 	return  deleteFromList$.merge(pushToList$).map( x => ({list: x}) ).throttle(100);
+	// } );
+
 }
